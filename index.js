af889ac5d69442c682771d71455bacca0fd15ec9
Fabien <fab.grignoux@gmail.com>
2021-12-15 18:53:40 +0100

    clear comments

--- index.js
+++ index.js
@@ -7,22 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// const allowedOrigins = [
-//   "http://localhost:5000",
-//   "http://localhost:5001",
-//   "http://localhost:5002",
-//   "http://localhost:8080",
-//   "http://localhost:8081",
-//   "http://localhost:3000",
-//   "http://localhost:3001",
-// ];
-
-// const options = {
-//   origin: allowedOrigins,
-// };
-
-//app.use(cors(options));
-
 import datas from "./src/datas.js";
 import Routes from "./src/routes.js";
 
